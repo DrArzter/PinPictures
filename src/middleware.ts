21358f282e7e4b2801df0cbc89806950918f25cf
DrArzter <chapegarostislav@gmail.com>
2024-10-21 10:34:35 +0200

    CORS settings set
    Co-authored-by: hymaxo <hymaxo@users.noreply.github.com>

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+
+import { NextResponse } from 'next/server';
+
+export function middleware(req: any) {
+    const res = NextResponse.next();
+
+    res.headers.set('Access-Control-Allow-Origin', '*');
+    res.headers.set('Access-Control-Allow-Credentials', 'true');
+    res.headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return new Response(null, {
+            status: 200,
+            headers: res.headers,
+        });
+    }
+
+    return res;
+}
